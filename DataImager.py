f3ec2898790942fa7e98cfb58d39a48cee63d996
eigenspek <eigenspek@users.noreply.github.com>
2017-08-01 22:07:17 +0200

    Add files via upload

---
+++ DataImager.py
@@ -0,0 +1,124 @@
+# coding: utf-8
+
+""" #### Developped by Dylan AMELOT, August 1st 2017 
+
+	This little library contains tools to convert a dataset into images """
+
+
+
+import os
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from scipy.misc import toimage
+
+
+def get_ft_weights(X):
+	""" Calculates the covariance matrix of Data and then perform EigenDecomposition to get EigenValues corresponding 
+		to relative covariance weights assigned to each feature"""
+	Xstd = StandardScaler().fit_transform(X)
+	cov_matrix = np.cov(Xstd.T) # Covariance Matrix
+	e_vals, e_vecs = np.linalg.eig(cov_matrix) # EigenDecomposition
+	tot = sum(e_vals)
+	var_exp = [(i / tot)*100 for i in e_vals]
+	print('relative weights summing-up to %s' % sum(var_exp))
+	return var_exp
+
+def transform_weights(l):
+	""" turns a df of float weights into a df of int weights for use as image width. """
+	l = np.array(l)
+	for i in range(len(l)):
+		l[i] = [int(round(l[i]))]
+		
+	return pd.DataFrame(l)
+
+
+def datatoimg(X, Xt, mode='1'):
+	""" Converts a dataset of type DataFrame to an ensemble of images. Please, pass X as type DataFrame. 
+	returns two numpy arrays each containing either train or test arrays of corresponding images. 
+	Modes : '1' is equal weight for each feature. 'w' each feature will have a width associated to its relative weight."""
+	features_number = X.shape[1]
+	img_height = getimgheight(X)
+	if mode == '1':
+		ximg,xtestimg = subdatatoimg(X,Xt,features_number,img_height,1)
+	elif mode == 'w':
+		ft_weights = get_ft_weights(X)
+		ft_weights = pd.DataFrame(ft_weights)
+		img_subwidths = transform_weights(ft_weights)
+		ximg,xtestimg = subdatatoimg(X,Xt,features_number, img_height,img_subwidths)
+	else : 
+		print('###### Error in entered mode, please ensure mode = "1" or "w" ######')
+	return np.array(ximg), np.array(xtestimg)
+
+def subdatatoimg(X,Xt, features_number, img_height, widths):
+	""" Actually does the conversion into images, is called by datatoimg(). """
+	os.chdir(os.getcwd() + '/dataimages/train')
+	b = X.values
+	ximg = []
+	ximg = list(ximg)
+	xtestimg =[] 
+	xtestimg = list(xtestimg)
+	for i in range(len(X.values)):
+		a = b[i]
+#		print(a)
+		imgm = imgmatrix(a, features_number, img_height, widths)
+		title = i
+		ximg.append([imgm])
+		img = toimage(imgm)
+		img.save('%s.png' % str(title))
+	print('Converted %d train data to images' % len(b))
+	os.chdir('../')
+	os.chdir(os.getcwd() + '/test')
+	c = Xt.values
+	for i in range(len(Xt.values)):
+		d = c[i]
+#		print(d)
+		imgmt = imgmatrix(d, features_number, img_height, widths)
+		title = len(b) + i
+		xtestimg.append([imgmt])
+		img = toimage(imgmt)
+		img.save('%s.png' % str(title))
+	print('Converted %d test data to images' % len(c))
+	os.chdir('../')
+	return ximg,xtestimg
+#-------------------------------------------
+
+def getimgheight(X):
+	""" searches in dataset all groups in each features and returns max(largest group of each features)  """
+	list_features = X.columns
+	list_features = list_features.values
+	ft_categs = []
+	for d in list_features:
+		ft_categs.append(max(np.array(X.groupby([d],as_index=False).size().index)))
+	print(ft_categs)
+	ft_max_categ = max(ft_categs)
+	print(ft_max_categ)
+	return ft_max_categ + 1
+
+def imgmatrix(X, ft_nbr, height, widths=1):
+	""" Here X is one object will all its features. Create a matrix corresponding to the image of this one object"""	
+	X = X.T	
+	m = np.empty([height,1], dtype=np.int)
+	if type(widths) != type(1):
+		w = widths.values
+		for i in range(ft_nbr):
+			a = w[i]
+			subm = imgsubmatrix(X[i], height, a)
+			m = np.append(m, subm, axis=1)
+	else : 
+		for i in range(ft_nbr):
+			subm = imgsubmatrix(X[i], height, 1)
+			m = np.append(m, subm, axis=1)
+	m = np.delete(m, 0, 1)	
+	return m
+
+
+def imgsubmatrix(X, height, width=1):
+	""" Here X is one feature of the input object and width if !=1 is the associated weight. Creates the submatrix for the 	part of the image associated to this feature """
+	X = int(X)
+	width = int(width)
+	height = int(height)
+	subm = np.zeros((height,width), dtype= np.int)
+	level = int(X)
+	subm[level,:]=1
+	return subm
---
+++ howtouse.py
@@ -0,0 +1,107 @@
+# coding: utf-8
+
+import os 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import DataImager as di
+from scipy.misc import toimage
+from sklearn.preprocessing import StandardScaler
+
+#import data
+training_set = pd.read_csv('~/your/path/to/train_set')
+testing_set = pd.read_csv('~/your/path/to/test_set')
+
+# prepare data
+# - Split features and label to X,y
+# - Remove from both train and test sets useless features 
+
+# At this point, your data X and X_test should have:
+# - no missing values
+# - all features categorized
+# - data mapped to integers. ( do it yourself or see sklearn.preprocessing.LabelEncoder() )
+# - X and X_test must be of DataFrame type, preferably. 
+
+
+# Be sure to use a os.chdir() to put the images you will create in a separate folder. 
+
+# ------------------------------------------------------------------------------
+# Verbosed way to convert Data to Image with widths = 1 
+
+# Getting the number of features : 
+features_number = X.shape[1] 
+
+# couting number of categories per feature and finding the maximum : 
+list_features = X.columns
+list_features = list_features.values
+ft_categs = []
+for d in list_features:
+	ft_categs.append(max(np.array(X.groupby([d],as_index=False).size().index)))
+print('highest categorie in each feature' , ft_categs)
+ft_max_categ = max(ft_categs)
+print('highest categorie overall features ', ft_max_categ)
+img_height = ft_max_categ + 1
+
+# turning only one data object into an image : 
+data = X.values
+test_image = di.imgmatrix(data[42], features_number, img_height, 1) # we take input object 42. 
+print(test_image) # here you can see the associated np.array 
+test_img = toimage(test_image)
+test_img.save('test_image.png')
+
+#-------------------------------------
+# If you want to use relative weights for widths of each feature, define the following 
+# and when calling di.imgmatrix replace 1 by img_subwidths. 
+
+# getting relative features weight 
+ft_weights = di.get_ft_weights(X)
+ft_weights = pd.DataFrame(ft_weights)
+# transforming features weight to get img-submatrices width
+img_subwidths = di.transform_weights(ft_weights)
+# ------------------------------------
+
+
+# Converting all train data into images and create a list with all images: 
+os.chdir(os.getcwd() + '/dataimages/train') # or whatever folder you want. 
+b = X.values
+ximg = []
+ximg = list(ximg)
+for i in range(len(X.values)):
+	a = b[i]
+	print(a)
+	imgm = di.imgmatrix(a, features_number, img_height,1)
+	ximg.append([imgm])	
+	title = i
+	img = toimage(imgm)
+	img.save('%s.png' % str(title))
+os.chdir('../')
+Ximg = np.array(ximg) # Here we convert our list into a np.array containing all arrays of each images into one big array. 
+print(Ximg[56]) # you can access one image like this
+
+# then you can do the same for test data, be sure to put it in a different folder, and in the loop
+# modify title to : title = len(b) + i , in order to name correctly images. 
+
+
+######################################################################################################################
+
+# Non-Verbosed way to do all of above : 
+
+# you need to provide both the train_set and test_set 
+
+# for equal widths for each feature : 
+Xi,Xti = di.datatoimg(X, Xtest,'1') 
+print(len(Xi),len(Xti))
+
+# if you want to use weighted widths for each feature : 
+Xi,Xti = di.datatoimg(X,Xtest,'w') 
+print(len(Xi),len(Xti)) 
+
+# note that Xi,Xti will be returned as numpy arrays. 
+print(Xi[42]) # to print image corresponding to object number 42. 
+
+
+
+
+
+
